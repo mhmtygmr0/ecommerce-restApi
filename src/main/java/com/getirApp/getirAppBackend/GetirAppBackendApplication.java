a6026b5c90f542b4755ff180199d150e798cf7aa
Mehmet <mehmetygmrr02@gmail.com>
2025-02-24 14:30:50 +0300

    İlk commit

---
+++ src/main/java/com/getirApp/getirAppBackend/GetirAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.getirApp.getirAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetirAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetirAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getirApp/getirAppBackend/GetirAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getirApp.getirAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetirAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
