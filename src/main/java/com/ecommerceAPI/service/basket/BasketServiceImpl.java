4085de97ff4ee02aa251446bcda8dffe61b6514a
Mehmet <mehmetygmrr02@gmail.com>
2025-04-13 16:22:21 +0300

    0

--- src/main/java/com/ecommerceAPI/service/basket/BasketServiceImpl.java
+++ src/main/java/com/ecommerceAPI/service/basket/BasketServiceImpl.java
@@ -3,7 +3,6 @@ package com.ecommerceAPI.service.basket;
 import com.ecommerceAPI.core.exception.NotFoundException;
 import com.ecommerceAPI.core.utils.Msg;
 import com.ecommerceAPI.entity.Basket;
-import com.ecommerceAPI.entity.User;
 import com.ecommerceAPI.repository.BasketRepository;
 import com.ecommerceAPI.service.user.UserService;
 import org.springframework.stereotype.Service;
@@ -25,19 +24,15 @@ public class BasketServiceImpl implements BasketService {
     @Override
     @Transactional
     public Basket save(Basket basket) {
-        Long userId = basket.getUser().getId();
-        User user = this.userService.getById(userId);
-
-        basket.setUser(user);
+        basket.setId(null);
+        basket.setUser(this.userService.getById(basket.getUser().getId()));
         basket.setTotalPrice(basket.getTotalPrice() != null ? basket.getTotalPrice() : 0.0);
-
         return this.basketRepository.save(basket);
     }
 
     @Override
     public Basket getById(Long id) {
-        return this.basketRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException(Msg.NOT_FOUND, "Basket"));
+        return this.basketRepository.findById(id).orElseThrow(() -> new NotFoundException(Msg.NOT_FOUND, "Basket"));
     }
 
     @Override
@@ -49,12 +44,8 @@ public class BasketServiceImpl implements BasketService {
     @Transactional
     public Basket update(Basket basket) {
         Basket existingBasket = this.getById(basket.getId());
-        Long userId = basket.getUser().getId();
-
-        User user = this.userService.getById(userId);
-        basket.setUser(user);
+        basket.setUser(this.userService.getById(basket.getUser().getId()));
         basket.setTotalPrice(basket.getTotalPrice() != null ? basket.getTotalPrice() : existingBasket.getTotalPrice());
-
         return this.basketRepository.save(basket);
     }
 
