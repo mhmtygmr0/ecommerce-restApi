829b9f8d15301a9e2756e9383fba005db3dce19a
Mehmet <mehmetygmrr02@gmail.com>
2025-04-15 23:56:58 +0300

    0

--- src/main/java/com/ecommerceAPI/service/category/CategoryService.java
+++ src/main/java/com/ecommerceAPI/service/category/CategoryService.java
@@ -1,17 +1,18 @@
 package com.ecommerceAPI.service.category;
 
 import com.ecommerceAPI.entity.Category;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface CategoryService {
-    Category save(Category category);
+    Category save(Category category, MultipartFile imageFile);
 
     Category getById(Long id);
 
     List<Category> getCategoryList();
 
-    Category update(Category category);
+    Category update(Category category, MultipartFile imageFile);
 
     void delete(Long id);
 }
