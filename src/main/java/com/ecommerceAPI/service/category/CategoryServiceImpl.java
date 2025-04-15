de7a78c510c2190e68d33feef654208376c74d40
Mehmet <mehmetygmrr02@gmail.com>
2025-04-15 23:57:07 +0300

    0

--- src/main/java/com/ecommerceAPI/service/category/CategoryServiceImpl.java
+++ src/main/java/com/ecommerceAPI/service/category/CategoryServiceImpl.java
@@ -1,13 +1,21 @@
 package com.ecommerceAPI.service.category;
 
+import com.ecommerceAPI.core.exception.ImageSaveException;
 import com.ecommerceAPI.core.exception.NotFoundException;
 import com.ecommerceAPI.core.utils.Msg;
 import com.ecommerceAPI.entity.Category;
 import com.ecommerceAPI.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -20,7 +28,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     @Transactional
-    public Category save(Category category) {
+    public Category save(Category category, MultipartFile imageFile) {
+        String imageUrl = saveImage(imageFile);
+        category.setImageUrl(imageUrl);
+
         return this.categoryRepository.save(category);
     }
 
@@ -36,8 +47,16 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     @Transactional
-    public Category update(Category category) {
+    public Category update(Category category, MultipartFile imageFile) {
         Category existingCategory = this.getById(category.getId());
+
+        if (imageFile != null) {
+            String imageUrl = saveImage(imageFile);
+            category.setImageUrl(imageUrl);
+        } else {
+            category.setImageUrl(existingCategory.getImageUrl());
+        }
+
         category.setProductList(existingCategory.getProductList());
         return this.categoryRepository.save(category);
     }
@@ -47,5 +66,22 @@ public class CategoryServiceImpl implements CategoryService {
     public void delete(Long id) {
         Category category = this.getById(id);
         this.categoryRepository.delete(category);
+    }
+
+    private String saveImage(MultipartFile imageFile) {
+        String uploadDir = "images/categoryImages/";
+        File folder = new File(uploadDir);
+        if (!folder.exists()) folder.mkdirs();
+
+        String fileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
+        Path filePath = Paths.get(uploadDir + fileName);
+
+        try {
+            Files.write(filePath, imageFile.getBytes());
+        } catch (IOException e) {
+            throw new ImageSaveException(Msg.IMAGE_SAVE_FAILED, e);
+        }
+
+        return "/images/categoryImages/" + fileName;
     }
 }
