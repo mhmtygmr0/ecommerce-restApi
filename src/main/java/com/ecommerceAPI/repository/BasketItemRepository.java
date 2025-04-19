4331beabf32dd642f7705eb5d376ef5ac8492b75
Mehmet <mehmetygmrr02@gmail.com>
2025-04-19 15:48:41 +0300

    0

--- src/main/java/com/ecommerceAPI/repository/BasketItemRepository.java
+++ src/main/java/com/ecommerceAPI/repository/BasketItemRepository.java
@@ -1,7 +1,10 @@
 package com.ecommerceAPI.repository;
 
 import com.ecommerceAPI.entity.BasketItem;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +12,9 @@ import java.util.List;
 @Repository
 public interface BasketItemRepository extends JpaRepository<BasketItem, Long> {
     List<BasketItem> findAllByOrderByIdAsc();
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM BasketItem b WHERE b.basket.id = :basketId")
+    void deleteByBasketId(Long basketId);
 }
