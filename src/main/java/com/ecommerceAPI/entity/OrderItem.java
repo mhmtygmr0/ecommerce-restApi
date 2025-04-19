82ca66a4760465d6901ee1e66bff9df11f3ebd25
Mehmet <mehmetygmrr02@gmail.com>
2025-04-19 15:49:28 +0300

    0

---
+++ src/main/java/com/ecommerceAPI/entity/OrderItem.java
@@ -0,0 +1,41 @@
+package com.ecommerceAPI.entity;
+
+import com.ecommerceAPI.core.utils.Msg;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "quantity")
+    @Min(value = 1, message = Msg.QUANTITY)
+    private Long quantity;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "total_price")
+    private Double totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
