92e54decd7c0d596ac1672a56be3433b3aca0dcc
Mehmet <mehmetygmrr02@gmail.com>
2025-04-19 15:48:45 +0300

    0

--- src/main/java/com/ecommerceAPI/dto/request/BasketItemRequest.java
+++ src/main/java/com/ecommerceAPI/dto/request/BasketItemRequest.java
@@ -1,12 +1,12 @@
 package com.ecommerceAPI.dto.request;
 
+import com.ecommerceAPI.core.utils.Msg;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import com.ecommerceAPI.core.utils.Msg;
 
 @Getter
 @Setter
@@ -14,13 +14,13 @@ import com.ecommerceAPI.core.utils.Msg;
 @AllArgsConstructor
 public class BasketItemRequest {
 
-    @NotNull(message = Msg.BASKET_ID_NULL)
+    @NotNull(message = Msg.BASKET_ITEM_ID_NULL)
     private Long basketId;
 
-    @NotNull(message = Msg.BASKET_PRODUCT_ID_NULL)
+    @NotNull(message = Msg.BASKET_ITEM_PRODUCT_ID_NULL)
     private Long productId;
 
-    @NotNull(message = Msg.BASKET_QUANTITY_NULL)
+    @NotNull(message = Msg.BASKET_ITEM_QUANTITY_NULL)
     @Min(value = 1, message = Msg.QUANTITY)
     private Long quantity;
 }
