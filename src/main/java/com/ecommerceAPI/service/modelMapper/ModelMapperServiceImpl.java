b29a002b4240c9bbf4a6f1e7752dd82cb29939f0
Mehmet <mehmetygmrr02@gmail.com>
2025-04-23 17:08:05 +0300

    0

--- src/main/java/com/ecommerceAPI/service/modelMapper/ModelMapperServiceImpl.java
+++ src/main/java/com/ecommerceAPI/service/modelMapper/ModelMapperServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ecommerceAPI.service.modelMapper;
 
+import com.ecommerceAPI.dto.response.OrderItemResponse;
+import com.ecommerceAPI.entity.OrderItem;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,12 @@ public class ModelMapperServiceImpl implements ModelMapperService {
     @Override
     public ModelMapper forResponse() {
         this.modelMapper.getConfiguration().setAmbiguityIgnored(true).setMatchingStrategy(MatchingStrategies.LOOSE);
+
+        modelMapper.typeMap(OrderItem.class, OrderItemResponse.class)
+                .addMapping(src -> src.getProduct().getName(), OrderItemResponse::setProductName)
+                .addMapping(src -> src.getProduct().getPrice(), OrderItemResponse::setProductPrice)
+                .addMapping(src -> src.getProduct().getImageUrl(), OrderItemResponse::setProductImageUrl);
+
         return this.modelMapper;
     }
 }
