097dc0b8f84d4ecdb959ec57123434ea713ebf13
Mehmet <mehmetygmrr02@gmail.com>
2025-05-08 22:29:58 +0300

    0

--- src/main/java/com/ecommerceAPI/controller/DeliveryController.java
+++ src/main/java/com/ecommerceAPI/controller/DeliveryController.java
@@ -1,13 +1,10 @@
 package com.ecommerceAPI.controller;
 
 import com.ecommerceAPI.core.utils.ResultHelper;
-import com.ecommerceAPI.dto.request.DeliveryRequest;
 import com.ecommerceAPI.dto.response.DeliveryResponse;
 import com.ecommerceAPI.entity.Delivery;
 import com.ecommerceAPI.service.delivery.DeliveryService;
 import com.ecommerceAPI.service.modelMapper.ModelMapperService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,13 +23,6 @@ public class DeliveryController {
         this.modelMapper = modelMapper;
     }
 
-    @PostMapping
-    public ResponseEntity<Map<String, Object>> save(@Valid @RequestBody DeliveryRequest deliveryRequest) {
-        Delivery delivery = this.modelMapper.forRequest().map(deliveryRequest, Delivery.class);
-        this.deliveryService.save(delivery);
-        return ResponseEntity.status(HttpStatus.CREATED).body(ResultHelper.created(this.modelMapper.forResponse().map(delivery, DeliveryResponse.class)));
-    }
-
     @GetMapping("/{id}")
     public ResponseEntity<Map<String, Object>> get(@PathVariable("id") Long id) {
         Delivery delivery = this.deliveryService.getById(id);
@@ -46,24 +36,10 @@ public class DeliveryController {
         return ResponseEntity.ok(ResultHelper.success(deliveryResponseList));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Map<String, Object>> update(@PathVariable("id") Long id, @Valid @RequestBody DeliveryRequest deliveryRequest) {
-        Delivery delivery = this.modelMapper.forRequest().map(deliveryRequest, Delivery.class);
-        delivery.setId(id);
-        this.deliveryService.update(delivery);
-        return ResponseEntity.ok(ResultHelper.success(this.modelMapper.forResponse().map(delivery, DeliveryResponse.class)));
-    }
-
     @PutMapping("/delivered/{id}")
     public ResponseEntity<Map<String, Object>> markAsDelivered(@PathVariable("id") Long deliveryId) {
         Delivery delivery = this.deliveryService.getById(deliveryId);
         this.deliveryService.markAsDelivered(deliveryId);
         return ResponseEntity.ok(ResultHelper.success(this.modelMapper.forResponse().map(delivery, DeliveryResponse.class)));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Map<String, Object>> delete(@PathVariable("id") Long id) {
-        this.deliveryService.delete(id);
-        return ResponseEntity.ok(ResultHelper.ok());
     }
 }
